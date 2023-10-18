7ce58f37c12f0f6d31ee885dc3f1ec1332dd7b10
acse-10bd39f6 <tg1523@ic.ac.uk>
2023-10-19 00:14:18 +0100

    fixed mistaske in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(name='acsefunctions',
       # Automatically set package version using git metadata
       use_scm_version=True,
       setup_requires=['setuptools_scm'],
-      
+
       description="Package for trigonometric functions etc.",
       author="Tehmoor",
       author_email='tehmoor.gull@imperial.ac.uk',
-      packages=['simple_functions'])
+      packages=['acsefunctions'])
