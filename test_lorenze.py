a7fea692382d3df094bdd6b72c16c0cc9d8a8281
acse-10bd39f6 <tg1523@ic.ac.uk>
2023-10-18 19:27:47 +0100

    Remove tolerance in unit tests

--- test_lorenze.py
+++ test_lorenze.py
@@ -7,11 +7,11 @@ from taylor import sin, cos, tan, exp
 def test_sin():
     assert np.allclose(sin(0), np.array([0.]))
     assert np.allclose(sin(np.pi/2), np.array([1.]))
-    assert np.allclose(sin(np.pi), np.array([0.]), atol=1e-5)
+    assert np.allclose(sin(np.pi), np.array([0.]))
 
 def test_cos():
     assert np.allclose(cos(0), np.array([1.]))
-    assert np.allclose(cos(np.pi/2), np.array([0.]), atol=1e-5)
+    assert np.allclose(cos(np.pi/2), np.array([0.]))
     assert np.allclose(cos(np.pi), np.array([-1.]))
 
 def test_tan():
@@ -19,6 +19,6 @@ def test_tan():
     assert np.allclose(tan(np.pi/4), np.array([1.]))
 
 def test_exp():
-    assert np.allclose(exp(1), np.array([np.exp(1)]), atol=1e-5)
+    assert np.allclose(exp(1), np.array([np.exp(1)]))
     assert np.allclose(exp(0), np.array([np.exp(0)]))
-    assert np.allclose(exp([-1,0,1]), np.exp(np.array([-1,0,1])), atol=1e-5)
+    assert np.allclose(exp([-1,0,1]), np.exp(np.array([-1,0,1])))
